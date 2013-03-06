d170be1bdbe0ac1d112900693ba8ed8e1e3234a2
yanniks <kontakt@yanniks.de>
2013-03-06 18:54:12 +0100

    source upload

---
+++ README.md
@@ -0,0 +1,4 @@
+yanniks.de Android App Source Code
+
+This is the source code of the yanniks.de Android App.
+Attention: Requies Holo Everywhere!
---
+++ src/de/yanniks/app/Start.java
@@ -0,0 +1,13 @@
+package de.yanniks.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Start extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.start);
+	}
+}
