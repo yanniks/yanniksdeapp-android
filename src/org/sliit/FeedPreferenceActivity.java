da50b1c6310f9d7a5387005ce63595dbb249198b
yanniks <kontakt@yanniks.de>
2013-03-18 13:29:05 -0400

    fixed settings tab - still not lucky with it

--- src/org/sliit/FeedPreferenceActivity.java
+++ src/org/sliit/FeedPreferenceActivity.java
@@ -2,8 +2,8 @@ package org.sliit;
 
 import android.os.Bundle;
 import android.preference.ListPreference;
+import android.preference.PreferenceActivity;
 
-import org.holoeverywhere.preference.PreferenceActivity;
 import org.sliit.domain.Feed;
 import org.sliit.service.RepositoryController;
 import de.yanniks.app.R;
@@ -36,7 +36,7 @@ public class FeedPreferenceActivity extends PreferenceActivity {
         
         addPreferencesFromResource(R.xml.preferences);
         
-        org.holoeverywhere.preference.ListPreference listPref = (org.holoeverywhere.preference.ListPreference) findPreference(PREF_START_CHANNEL_KEY);
+        ListPreference listPref = (ListPreference) findPreference(PREF_START_CHANNEL_KEY);
         
         List<Feed> feeds = mRepositoryController.getFeeds();
         Iterator<Feed> feedIterator = feeds.iterator();
