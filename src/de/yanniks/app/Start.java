be13fa73e0ad73e175e8cdc6a14721e5d191f85d
yanniks <kontakt@yanniks.de>
2013-04-03 10:46:33 +0200

    removed unneeded files, updated compatibility to Android 2.x

--- src/de/yanniks/app/Start.java
+++ src/de/yanniks/app/Start.java
@@ -1,9 +1,16 @@
 package de.yanniks.app;
 
 import android.os.Bundle;
-import org.sliit.SplashScreenActivity;
 import android.view.View;
+
 import org.holoeverywhere.app.Activity;
+import org.sliit.SplashScreenActivity;
+import org.sliit.service.SharedPreferencesHelper;
+
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
+
 import android.content.Intent;
 
 public class Start extends Activity {
@@ -13,6 +20,21 @@ public class Start extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.start);
 	}
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu) {
+        MenuInflater inflater = getSupportMenuInflater();
+        inflater.inflate(R.menu.menu_home, menu);
+        return super.onCreateOptionsMenu(menu);
+    }  
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_opt_about){
+        	showDialog(SharedPreferencesHelper.DIALOG_ABOUT);
+        	return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
     public void impress (final View view) {
     	startActivity (new Intent (this, impress.class));
     }
