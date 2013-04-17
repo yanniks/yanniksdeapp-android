7ace050a234723db5e96365c8458225c98f117d7
yanniks <kontakt@yanniks.de>
2013-04-17 21:51:10 +0200

    final changes for Google Play version 4.0.2

--- src/de/yanniks/app/Start.java
+++ src/de/yanniks/app/Start.java
@@ -1,6 +1,7 @@
 package de.yanniks.app;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import org.holoeverywhere.app.Activity;
@@ -11,6 +12,9 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 
 public class Start extends Activity {
@@ -40,5 +44,56 @@ public class Start extends Activity {
     }
     public void blog (final View view) {
     	startActivity (new Intent (this, SplashScreenActivity.class));
+    }
+    @Override
+    protected Dialog onCreateDialog(int id) {
+    	Dialog dialog = null;
+    	CharSequence title = null;
+    	LayoutInflater inflater = null;
+    	View dialogLayout = null;
+    	AlertDialog.Builder builder = null;
+        switch (id) {
+        	case SharedPreferencesHelper.DIALOG_ABOUT:
+        		//title = getResources().getText(R.string.app_name) + " - " + getResources().getText(R.string.version) + " " + SharedPreferencesHelper.getVersionName(this);
+	        	title = getString(R.string.app_name) + " - " + getString(R.string.version) + " " + SharedPreferencesHelper.getVersionName(this);
+	        	
+	        	/*
+	        	 * Without cancel button
+	        	dialog = new Dialog(this);
+	        	dialog.setContentView(R.layout.dialog_about);
+	        	dialog.setTitle(title);
+	        	*/
+        		inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        		dialogLayout = inflater.inflate(R.layout.dialog_about, null);
+        		builder = new AlertDialog.Builder(this);
+        		builder.setView(dialogLayout)
+        			   .setTitle(title)
+        			   .setIcon(R.drawable.ic_launcher)
+        			   .setNeutralButton(R.string.close, new DialogInterface.OnClickListener() {
+        		           public void onClick(DialogInterface dialog, int id) {
+        		                dialog.cancel();
+        		           }
+        		       });
+        		dialog = builder.create();
+	        	break;
+        	case SharedPreferencesHelper.DIALOG_NO_CONNECTION:
+        		title = getString(R.string.error);
+        		inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        		dialogLayout = inflater.inflate(R.layout.dialog_no_connection, null);
+        		builder = new AlertDialog.Builder(this);
+        		builder.setView(dialogLayout)
+        			   .setTitle(title)
+        			   .setIcon(R.drawable.ic_launcher)
+        			   .setNeutralButton(R.string.ok, new DialogInterface.OnClickListener() {
+        		           public void onClick(DialogInterface dialog, int id) {
+        		                dialog.cancel();
+        		           }
+        		       });
+        		dialog = builder.create();
+        		break;
+            default:
+            	dialog = null;
+        }
+        return dialog;
     }
 }
